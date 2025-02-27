5c2f5bb07567d2818e72ad59140b0d5edff6db8e
Sahil Hussain <sahilhussainofficial@gmail.com>
2025-02-27 20:49:06 +0530

    selection-sort done

--- Arrays/e_selectionSort.cpp
+++ Arrays/e_selectionSort.cpp
@@ -7,18 +7,24 @@ int main(){
     cout<<"Enter array size: ";
     cin>>size;
     
-    int arr[size];d
+    int arr[size];
     cout<<"Enter "<<size<<" elements: "<<endl;
     for(int i=0; i<size; i++){
         cin>>arr[i];
     }
 
-    cout<<"Array before sorting: "<<endl;
+    cout<<"Array after sorting: ";
+    for(int i=0; i<size-1; i++){
+        int index = i;
+        for(int j=i+1; j<size; j++){
+            if(arr[j]<arr[index]){
+                index = j;
+            }
+            swap(arr[i], arr[index]);
+        }
+    }
     for(int i=0; i<size; i++){
         cout<<arr[i]<<" ";
     }
-
-    cout<<"Array after sorting: ";
-    int j;
 
 }
