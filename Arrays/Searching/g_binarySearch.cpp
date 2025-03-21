a97de282f6f9e68b5c8bb40f0296b9cf94b84230
Sahil Hussain <sahilhussainofficial@gmail.com>
2025-03-22 00:00:24 +0530

    binary-search

--- Arrays/Searching/g_binarySearch.cpp
+++ Arrays/Searching/g_binarySearch.cpp
@@ -1,33 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int arr[1000], size;
-    cout<<"Enter array size: ";
-    cin>>size;
-    
-    cout<<"Enter "<<size<<" elements: "<<endl;
-    cout<<"(Note: Array must be sorted)";
-    for(int i=0; i<size; i++){
-        cin>>arr[i];
-    }
-
-    cout<<"Array: ";
-    for(int i=0; i<size; i++){
-        cout<<arr[i]<<" ";
-    }
-
-    int start=0; int end=size-1; int mid = (start+end)/2; int key;
-    cout<<"Element to find: ";
-    cin>>key;
-
+int binarySearch(int arr[],int n,int key){
+    int start=0, end=n-1, mid;
     while(start<=end){
-        mid = (start+end)/2;
-        if(arr[mid]==key) break;
-        else if(key>arr[mid]) start=mid+1;
+        mid = start + (end-start)/2;
+        if(arr[mid]==key) return mid;
+        else if (arr[mid]<key) start = mid+1;
         else end = mid-1;
     }
-    cout<<"The position of "<<key<<" is "<<mid;
+}
+
+int main(){
+    int arr[1000], n, key;
+    cout<<"Size: ";
+    cin>>n;
+    for(int i=0; i<n; i++)
+    cin>>arr[i];
+
+    cout<<"Find element: ";
+    cin>>key;
+
+    cout<<binarySearch(arr, n, key);
     
     return 0;
 }
