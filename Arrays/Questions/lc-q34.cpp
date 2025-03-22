bb1ddd27fcf9e3716ed11ef4271806768c2c7246
Sahil Hussain <sahilhussainofficial@gmail.com>
2025-03-23 00:09:15 +0530

    folder-fix

--- Arrays/Questions/lc-q34.cpp
+++ Arrays/Questions/lc-q34.cpp
@@ -3,27 +3,33 @@ using namespace std;
 
 int main(){
     int arr[5] = {3, 5, 5, 5, 7};
-    int start = 0; end = 4; mid, first=-1, last = -1;
+    int start = 0, end = 4, mid, first=-1, last = -1, target;
+    cout<<"Array: ";
+    for(int i=0; i<5; i++) cout<<arr[i]<<" ";
+    cout<<"Find: ";
+    cin>>target;
 
     //Find First and Last Position of Element in Sorted Array
     while(start<=end){
         mid = start+(end-start)/2;
-        if(nums[mid]==target)
+        if(arr[mid]==target)
         {first = mid;
         end = mid-1;}
-        else if(nums[mid]<target)
+        else if(arr[mid]<target)
         start = mid+1;
         else end = mid-1;
     }
     //last position
-    start =0; end=nums.size()-1;
+    start =0; end=4;
     while(start<=end){
         mid = start+(end-start)/2;
-        if(nums[mid]==target)
+        if(arr[mid]==target)
         {last = mid;
         start = mid+1;}
-        else if(nums[mid]<target)
+        else if(arr[mid]<target)
         start = mid+1;
         else end = mid-1;
     }
+    cout<<"first appearance: "<<first<<endl;
+    cout<<"last appearance: "<<last<<endl;
 }
